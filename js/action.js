508e1857ec4bb07bfc6024779f5bb7b3ebb31643
victor waswa <eunawaswa@gmail.com>
2018-07-26 12:26:27 +0300

    added my js

--- js/action.js
+++ js/action.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
     $("#d").click(function(event){
    event.preventDefault();
-   $(".d").show();
+   $(".d").slideToggle();
 
+    })
+})
+$(document).ready(function(){
+    $("#d1").click(function(event){
+   event.preventDefault();
+   $(".d2").slideToggle();
+
+    })
+})
+$(document).ready(function(){
+    $("#h01").click(function(event){
+   event.preventDefault();
+   $(".h02").slideToggle();
+
+    })
+})
+$(document).ready(function(){
+    $("#g01").click(function(event){
+   event.preventDefault();
+   $(".g02").slideToggle();
     })
 })
