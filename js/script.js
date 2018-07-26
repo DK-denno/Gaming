a6a7ea97cc403c973939b272c184c0ad9c8d4a86
dk-denno <dennisveer27@gmail.com>
2018-07-26 11:24:27 +0300

    removed errors

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+$(document).ready(function() {
+        $("#submit").click(function(event) {
+            event.preventDefault();
+          var commentInput = $("input.comm").val();
+          $("#comment").prepend('<li>'+ commentInput + '</li>');
+          alert('Your comment has been appreciated');
 
+        });
+      });
