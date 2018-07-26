9d7ca531e106a116eca551ae2b8f41ca4f58316d
billowbashir <billowbashir@gmail.com>
2018-07-26 13:13:42 +0300

    use google fonts,Add a gotop button

--- js/racing.js
+++ js/racing.js
@@ -28,6 +28,23 @@ $(document).ready(function(){
     event.preventDefault();
     $("#previewmario").slideToggle();
   })
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+      if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+          document.getElementById("gotop").style.display = "block";
+      } else {
+          document.getElementById("gotop").style.display = "none";
+      }
+  }
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+$("#gotop").click(function(event){
+  event.preventDefault();
+  topFunction();
+})
 //
 // $(".asphalt8").click(function() {
 //   window.location = $(this).find("a").attr("href");
