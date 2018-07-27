eacf3e980484b7f48c03b848834251472e9c4aef
dk-denno <dennisveer27@gmail.com>
2018-07-27 09:56:33 +0300

    added ufcko.html

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
         $("#submit").click(function(event) {
             event.preventDefault();
           var commentInput = $("input.comm").val();
-          $("#comment").prepend('<li>'+ commentInput + '</li>');
+          var emailInput=$("input#email").val();
+          $("#comment").prepend('<li>'+'<b>' + emailInput + '</b>' + ': '+ commentInput + '</li>');
           alert('Your comment has been appreciated');
 
         });
